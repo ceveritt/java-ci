53cd873e7c9b91ac098ef528ffd163e641a2dce8
Chris Everitt <chrisjameseveritt@gmail.com>
2016-05-31 08:23:33 -0400

    added null checking

--- src/main/java/com/example/ci/Example.java
+++ src/main/java/com/example/ci/Example.java
@@ -2,9 +2,12 @@ package com.example.ci;
 
 public class Example {
 
-	public String stringReverse(String s){
-		StringBuffer sb =  new StringBuffer(s.length());
-		for(int i = s.length() - 1; i >= 0; i--){
+	public String stringReverse(String s) {
+		if (s == null) {
+			return null;
+		}
+		StringBuffer sb = new StringBuffer(s.length());
+		for (int i = s.length() - 1; i >= 0; i--) {
 			sb.append(s.charAt(i));
 		}
 		return sb.toString();
